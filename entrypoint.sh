6f0afc0badcffae999556a730d6d2fb5ff615bc5
Jan Drees <jan.drees@wago.com>
2023-11-08 13:19:13 +0100

    change

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ then
     # build the mta archive and deploy it to the BTP
     mbt build -p=cf -t mta_archives --mtar=ui5app.mtar
     cf login -a $2 -o $3 -u $4 -p $5
+    cf install-plugin multiapps -f
     cf deploy ./mta_archives/ui5app.mtar -f
     exit 0
 else
