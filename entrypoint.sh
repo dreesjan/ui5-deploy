7ff13f449e8d9fd82ce043bf653df4f4ff16a1c0
Jan Drees <jan.drees@wago.com>
2023-11-08 10:23:49 +0100

    change in script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -l
 
 # BTP Deployment
 if [ $1 -eq 'btp' ]
@@ -7,6 +7,8 @@ then
     mbt build --mtar -p=cf -t ui5app.mtar
     cf login -a $2 -u $3 -p $4
     cf deploy ui5app.mtar -f
+    exit 0
 else
     echo "Please provide valid deployment type."
+    exit 1
 fi
