06aad0c96fafefd523d50e8c2c64690c40f79a96
Jan Drees <jan.drees@wago.com>
2023-11-15 08:21:00 +0100

    change

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,11 @@
 if [ "$1" = "btp" ]
 then
     # build the mta archive and deploy it to the BTP
-    mbt build -p=cf -t mta_archives --mtar=ui5app.mtar
+    mbt build -p=cf -t mta_archives --mtar=app.mtar
     cf login -a $2 -o $3 -u $4 -p $5
-    # cf install-plugin multiapps -f
-    cf deploy ./mta_archives/ui5app.mtar -f
+    cf deploy ./mta_archives/app.mtar -f
     exit 0
 else
-    echo "Please provide valid deployment type."
+    echo "Error: Please check deployment type!"
     exit 1
 fi
